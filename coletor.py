0ce09b3237d2456d82ead492badf00c4e0d1d6cc
Lucas Siqueira <lucassouza.ti@gmail.com>
2017-08-12 20:56:52 -0300

    implementado classificao 2 com alteracao no banco

--- coletor.py
+++ coletor.py
@@ -4,6 +4,8 @@ import scrapy
 import urlparse
 import peewee
 from model import Ente
+from selenium import webdriver
+import random
 
 class Contrato(scrapy.Item):
     unidade_gestora =scrapy.Field()
@@ -30,22 +32,71 @@ class ItemContrato(scrapy.Item):
     valor_unitario =scrapy.Field()
     valor_total =scrapy.Field()
 
+
+    #Dividir o algoritmos por cidades?
+    #Ou fazer um algoritmo genérico?
+
+    #Cidades que não tem nada: Cambuci, Italva, Itaocara, Porciúncula
+    #Cidades com informações básicas mas sem resultado: Laje do Muriaé, Miracema, Natividade, Varre-sai
+    #Cidades com infomrações sobre licitações e contratos, mas em pdf:Aperibé, Bom Jesus do Itabopana, Santo Antonio de Padua, São jose de Uba
+    #Cidades com informações em formato aberto, porem desatualizado: Itaperuna
+
 class Coletor(scrapy.Spider):
     name = 'coletor'
     start_urls = ['http://prefeituradecambuci.rj.gov.br/transparencia/licitacoes/']
+    entes = []
+    def __init__(self):
+        self.driver = webdriver.Firefox()
+        self.entes = Ente.select()  
 
     def parse(self, response):
+        
+        self.log(self.entes)
+        for ente in self.entes:
+            self.log(ente.link_licitacoes)
+            request = scrapy.Request(ente.link_licitacoes, self.classificacao_1)
+            request.meta['ente'] = ente
+            yield request
+            # ente.classificacao = classificacao
+            # ente.save()
+
+
+    def classificacao_1(self, response):
+        
+        #teste com selenium
+        
+        
+        # resultado = self.driver.find_elements_by_xpath("//div[@id='content_page_1']//a[@href='licitacao-310']")
+
+
+        #Procurar por algum link que conhetam "%dita" e termine com "pdf"
+        resultado = response.xpath("//a[re:test(@href, '.*dita.*pdf') or re:test(@href, '.*licita.*')]/@href").extract()
+        # resultado = response.xpath("//a[re:test(@href, '.*licita.*')]")
+        self.log(resultado)
         try:
-            Ente.create_table()
-        except peewee.OperationalError:
-            print 'Tabela Ente ja existe!'
+            request = scrapy.Request(random.choice(resultado), self.procura_por_editais)
+            request.meta['ente'] = response.meta['ente']
+            yield request
+        except:
+            self.driver.get(response.url)
+            resultado = self.driver.find_elements_by_partial_link_text("reg")
+            # for item in resultado:
+            #     self.log(item.get_attribute('href'))
+            request = scrapy.Request(random.choice(resultado).get_attribute('href'), self.procura_por_editais)
+            request.meta['ente'] = response.meta['ente']
+            yield request
+                
+            # print(resultado)
 
-        #Dividir o algoritmos por cidades?
-        #Ou fazer um algoritmo genérico?
 
-        #Cidades que não tem nada: Cambuci, Italva, Itaocara, Porciúncula
-        #Cidades com informações básicas mas sem resultado: Laje do Muriaé, Miracema, Natividade, Varre-sai
-        #Cidades com infomrações sobre licitações e contratos, mas em pdf:Aperibé, Bom Jesus do Itabopana, Santo Antonio de Padua, São jose de Uba
-        #Cidades com informações em formato aberto, porem desatualizado: Itaperuna
+            self.driver.close()
+        # return None
 
-
+    def procura_por_editais(self, response):
+        self.log(response.status)
+        ente = response.meta['ente']
+        if(response.status == 200):
+            ente.classificacao = 2
+            ente.save()
+        # self.driver.get(response.url)
+                
--- gera_banco.py
+++ gera_banco.py
@@ -9,17 +9,28 @@ if __name__ == '__main__':
 	except peewee.OperationalError:
 		print 'Tabela Ente ja existe!'
 
-	# cambuci = {
-	#     'nome': 'Prefeitura Municipal de Cambuci', 
- #        'municipio': 'Cambuci', 
- #        'link_transparencia': 'teste', 
- #        'link_licitacoes': 'http://prefeituradecambuci.rj.gov.br/transparencia/licitacoes/', 
- #        'link_contratos': 'teste', 
- #        'esfera': 'Municipal', 
- #        'classificacao': 1, 
- #        'ativo': 1
- #    }
-	# entes = [cambuci]
-	# Ente.insert_many(entes).execute()
+	cambuci = {
+	    'nome': 'Prefeitura Municipal de Cambuci', 
+        'municipio': 'Cambuci', 
+        'link_transparencia': 'teste', 
+        'link_licitacoes': 'http://prefeituradecambuci.rj.gov.br/transparencia/licitacoes/editais/', 
+        'link_contratos': 'teste', 
+        'esfera': 'Municipal', 
+        'classificacao': 1, 
+        'ativo': 1
+    }
+	italva = {
+		'nome': 'Prefeitura Municipal de Italva', 
+        'municipio': 'Italva', 
+        'link_transparencia': 'teste', 
+        'link_licitacoes': 'http://www.italva.rj.gov.br/ver_licitacoes', 
+        'link_contratos': 'http://www.italva.rj.gov.br/ver_licitacoes', 
+        'esfera': 'Municipal', 
+        'classificacao': 1, 
+        'ativo': 1
+    }
+	entes = [cambuci, italva]
+
+	Ente.insert_many(entes).execute()
 
 	
